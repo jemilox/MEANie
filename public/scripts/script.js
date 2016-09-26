2132bec2ea453bdabbd6b98c0b6250ca8c808712
Lisa Cook <jemilox@gmail.com>
2016-09-26 13:51:01 -0500

    display works

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,8 @@
 var myApp=angular.module( 'myApp', [] );
 
 myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http ){
+  $scope.allTheRecords = [];
+
   $scope.addRecord = function(){
     event.preventDefault();
     var objectToSend ={
@@ -18,11 +20,12 @@ myApp.controller( 'whereMyPeeps', [ '$scope', '$http', function( $scope, $http )
 };//end addRecord
 
 $scope.getRecords = function(){
+  $scope.allTheRecords = [];
   $http({
     method: 'GET',
     url: '/getRecords',
   }).then( function( response ){
-    $scope.allTheRecords = response;
+    $scope.allTheRecords = response.data;
     console.log( $scope.allTheRecords );
   }, function myError( response ){
     console.log( response.statusText );
